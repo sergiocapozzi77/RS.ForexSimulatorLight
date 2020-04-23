9ac2aeb64578ad549e4e30d90a238e7d8c226788
Sergio Capozzi <sergiocapozzi@outlook.com>
2020-04-23 10:20:32 +0100

    saved assemly

--- RS.Trading.ForexSimulator/Properties/AssemblyInfo.cs
+++ RS.Trading.ForexSimulator/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RS.Trading.ForexSimulator")]
+[assembly: AssemblyTitle("RS Forex Simulator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Racing Solutions Ltd")]
